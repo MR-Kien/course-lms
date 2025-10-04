31634095f1ca4212a3d00e7636c605a63fffe0dd
Khuongkun48 <khuonggm4869@gmail.com>
2025-10-04 23:26:22 +0700

    part4

--- src/modules/info/features/index.jsx
+++ src/modules/info/features/index.jsx
@@ -1,6 +1,6 @@
-import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
-import Profile from "./Profile";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Profile from "../components/Profile";
 
 export default function Index() {
   return (
