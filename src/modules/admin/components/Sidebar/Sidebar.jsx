a30086c251463a79d2dc897e5e2dc0e273c4c7df
Khuongkun48 <khuonggm4869@gmail.com>
2025-10-05 09:24:31 +0700

    part8

--- src/modules/admin/components/Sidebar/Sidebar.jsx
+++ src/modules/admin/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { ENDPOINTS } from "../../../routes/endPoints";
+import { ENDPOINTS } from "../../../../routes/endPoints";
 import { 
   BookOpen, 
   Users, 
